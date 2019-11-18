2f7263ce9f8d9aa320dd0520ba6909d029944c15
david-cortes <david.cortes.rivera@gmail.com>
2019-11-18 12:03:14 +0200

    bump python version

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ class build_ext_subclass( build_ext ):
 setup(
     name  = "isotree",
     packages = ["isotree"],
-    version = '0.1.4',
+    version = '0.1.5',
     description = 'Isolation-Based Outlier Detection, Distance, and NA imputation',
     author = 'David Cortes',
     author_email = 'david.cortes.rivera@gmail.com',
