0a9d3143680f61d3df47a796880bd082d5cd30b2
David Cortes <david.cortes.rivera@gmail.com>
2021-07-17 01:16:00 -0400

    fix incorrect distance calculation

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ if platform[:3] != "dar":
 setup(
     name  = "isotree",
     packages = ["isotree"],
-    version = '0.2.10',
+    version = '0.2.11',
     description = 'Isolation-Based Outlier Detection, Distance, and NA imputation',
     author = 'David Cortes',
     author_email = 'david.cortes.rivera@gmail.com',
