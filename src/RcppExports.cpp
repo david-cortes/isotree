347a3f4f098972d42c3a138e6afea12238367e6a
David Cortes <david.cortes.rivera@gmail.com>
2021-09-01 21:09:20 -0400

    replace deprecated configure macro

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // deserialize_IsoForest
 SEXP deserialize_IsoForest(Rcpp::RawVector src);
 RcppExport SEXP _isotree_deserialize_IsoForest(SEXP srcSEXP) {
