008dc81855f1fc485be7ed7ce8dc3e0d95e1a150
david-cortes <david.cortes.rivera@gmail.com>
2023-03-19 19:12:42 +0100

    improve python setup script

--- README.md
+++ README.md
@@ -138,7 +138,7 @@ pip install --no-use-pep517 isotree
 ```
 brew install libomp
 ```
-And then reinstall this package: `pip install --force-reinstall isotree`.
+And then reinstall this package: `pip install --upgrade --no-deps --force-reinstall isotree`.
 
 ** *
 **IMPORTANT:** the setup script will try to add compilation flag `-march=native`. This instructs the compiler to tune the package for the CPU in which it is being installed (by e.g. using AVX instructions if available), but the result might not be usable in other computers. If building a binary wheel of this package or putting it into a docker image which will be used in different machines, this can be overriden either by (a) defining an environment variable `DONT_SET_MARCH=1`, or by (b) manually supplying compilation `CFLAGS` as an environment variable with something related to architecture. For maximum compatibility (but slowest speed), it's possible to do something like this:
@@ -288,10 +288,6 @@ cmake -DNO_TEMPLATED_VERSIONS=1 ..
 ```
 (this option is not available for the Python build system)
 
-
-# Help wanted
-
-The package does not currenly have any functionality for visualizing trees. Pull requests adding such functionality would be welcome.
 
 # References
 
--- setup.py
+++ setup.py
@@ -17,6 +17,17 @@ def set_omp_false():
     global found_omp
     found_omp = False
 
+## Modify this to make the output of the compilation tests more verbose
+silent_tests = not (("verbose" in sys.argv)
+                    or ("-verbose" in sys.argv)
+                    or ("--verbose" in sys.argv))
+
+## Workaround for python<=3.9 on windows
+try:
+    EXIT_SUCCESS = os.EX_OK
+except AttributeError:
+    EXIT_SUCCESS = 0
+
 ## https://stackoverflow.com/questions/724664/python-distutils-how-to-get-a-compiler-that-is-going-to-be-used
 class build_ext_subclass( build_ext ):
     def build_extensions(self):
@@ -31,13 +42,13 @@ class build_ext_subclass( build_ext ):
 
         if is_msvc:
             for e in self.extensions:
-                e.extra_compile_args = ['/openmp', '/O2', '/std:c++14', '/fp:except-', '/wd4244', '/wd4267', '/wd4018', '/wd5030']
+                e.extra_compile_args = ['/openmp', '/O2', '/GL', '/std:c++14', '/fp:except-', '/wd4244', '/wd4267', '/wd4018', '/wd5030']
                 e.define_macros += [("NO_LONG_DOUBLE", None)]
                 if has_robinmap:
                     e.define_macros += [("_USE_ROBIN_MAP", None)]
         
         else:
-            if not self.check_for_variable_dont_set_march() and not self.is_arch_in_cflags():
+            if not self.check_for_variable_dont_set_march() and not self.check_cflags_contain_arch():
                 self.add_march_native()
             self.add_openmp_linkage()
             self.add_restrict_qualifier()
@@ -92,25 +103,41 @@ class build_ext_subclass( build_ext ):
 
         build_ext.build_extensions(self)
 
+    def check_cflags_contain_arch(self):
+        if ("CFLAGS" in os.environ) or ("CXXFLAGS" in os.environ):
+            has_cflags = "CFLAGS" in os.environ
+            has_cxxflags = "CXXFLAGS" in os.environ
+            arch_list = [
+                "-march", "-mcpu", "-mtune", "-msse", "-msse2", "-msse3",
+                "-mssse3", "-msse4", "-msse4a", "-msse4.1", "-msse4.2",
+                "-mavx", "-mavx2", "-mavx512"
+            ]
+            for flag in arch_list:
+                if has_cflags and flag in os.environ["CFLAGS"]:
+                    return True
+                if has_cxxflags and flag in os.environ["CXXFLAGS"]:
+                    return True
+        return False
+
     def check_for_variable_dont_set_march(self):
         return "DONT_SET_MARCH" in os.environ
 
     def add_march_native(self):
-        arg_march_native = "-march=native"
-        arg_mcpu_native = "-mcpu=native"
-        if self.test_supports_compile_arg(arg_march_native):
-            for e in self.extensions:
-                e.extra_compile_args.append(arg_march_native)
-        elif self.test_supports_compile_arg(arg_mcpu_native):
-            for e in self.extensions:
-                e.extra_compile_args.append(arg_mcpu_native)
+        args_march_native = ["-march=native", "-mcpu=native"]
+        for arg_march_native in args_march_native:
+            if self.test_supports_compile_arg(arg_march_native):
+                for e in self.extensions:
+                    e.extra_compile_args.append(arg_march_native)
+                break
 
     def add_link_time_optimization(self):
-        arg_lto = "-flto"
-        if self.test_supports_compile_arg(arg_lto):
-            for e in self.extensions:
-                e.extra_compile_args.append(arg_lto)
-                e.extra_link_args.append(arg_lto)
+        args_lto = ["-flto=auto", "-flto"]
+        for arg_lto in args_lto:
+            if self.test_supports_compile_arg(arg_lto):
+                for e in self.extensions:
+                    e.extra_compile_args.append(arg_lto)
+                    e.extra_link_args.append(arg_lto)
+                break
 
     def add_no_math_errno(self):
         arg_fnme = "-fno-math-errno"
@@ -128,36 +155,56 @@ class build_ext_subclass( build_ext ):
 
     def add_openmp_linkage(self):
         arg_omp1 = "-fopenmp"
-        arg_omp2 = "-qopenmp"
-        arg_omp3 = "-xopenmp"
-        arg_omp4 = "-fiopenmp"
+        arg_omp2 = "-fopenmp=libomp"
+        args_omp3 = ["-fopenmp=libomp", "-lomp"]
+        arg_omp4 = "-qopenmp"
+        arg_omp5 = "-xopenmp"
+        is_apple = sys.platform[:3].lower() == "dar"
         args_apple_omp = ["-Xclang", "-fopenmp", "-lomp"]
         args_apple_omp2 = ["-Xclang", "-fopenmp", "-L/usr/local/lib", "-lomp", "-I/usr/local/include"]
+        has_brew_omp = False
+        if is_apple:
+            res_brew_pref = subprocess.run(["brew", "--prefix", "libomp"], capture_output=silent_tests)
+            if res_brew_pref.returncode == EXIT_SUCCESS:
+                has_brew_omp = True
+                brew_omp_prefix = res_brew_pref.stdout.decode().strip()
+                args_apple_omp3 = ["-Xclang", "-fopenmp", f"-L{brew_omp_prefix}/lib", "-lomp", f"-I{brew_omp_prefix}/include"]
+
+
         if self.test_supports_compile_arg(arg_omp1, with_omp=True):
             for e in self.extensions:
                 e.extra_compile_args.append(arg_omp1)
                 e.extra_link_args.append(arg_omp1)
-        elif (sys.platform[:3].lower() == "dar") and self.test_supports_compile_arg(args_apple_omp, with_omp=True):
+        elif is_apple and self.test_supports_compile_arg(args_apple_omp, with_omp=True):
             for e in self.extensions:
                 e.extra_compile_args += ["-Xclang", "-fopenmp"]
                 e.extra_link_args += ["-lomp"]
-        elif (sys.platform[:3].lower() == "dar") and self.test_supports_compile_arg(args_apple_omp2, with_omp=True):
+        elif is_apple and self.test_supports_compile_arg(args_apple_omp2, with_omp=True):
             for e in self.extensions:
                 e.extra_compile_args += ["-Xclang", "-fopenmp"]
                 e.extra_link_args += ["-L/usr/local/lib", "-lomp"]
                 e.include_dirs += ["/usr/local/include"]
+        elif is_apple and has_brew_omp and self.test_supports_compile_arg(args_apple_omp3, with_omp=True):
+            for e in self.extensions:
+                e.extra_compile_args += ["-Xclang", "-fopenmp"]
+                e.extra_link_args += [f"-L{brew_omp_prefix}/lib", "-lomp"]
+                e.include_dirs += [f"{brew_omp_prefix}/include"]
         elif self.test_supports_compile_arg(arg_omp2, with_omp=True):
             for e in self.extensions:
-                e.extra_compile_args.append(arg_omp2)
-                e.extra_link_args.append(arg_omp2)
-        elif self.test_supports_compile_arg(arg_omp3, with_omp=True):
+                e.extra_compile_args += ["-fopenmp=libomp"]
+                e.extra_link_args += ["-fopenmp"]
+        elif self.test_supports_compile_arg(args_omp3, with_omp=True):
             for e in self.extensions:
-                e.extra_compile_args.append(arg_omp3)
-                e.extra_link_args.append(arg_omp3)
+                e.extra_compile_args += ["-fopenmp=libomp"]
+                e.extra_link_args += ["-fopenmp", "-lomp"]
         elif self.test_supports_compile_arg(arg_omp4, with_omp=True):
             for e in self.extensions:
                 e.extra_compile_args.append(arg_omp4)
                 e.extra_link_args.append(arg_omp4)
+        elif self.test_supports_compile_arg(arg_omp5, with_omp=True):
+            for e in self.extensions:
+                e.extra_compile_args.append(arg_omp5)
+                e.extra_link_args.append(arg_omp5)
         else:
             set_omp_false()
 
@@ -168,19 +215,15 @@ class build_ext_subclass( build_ext ):
                 e.extra_compile_args.append(O3)
 
     def add_highest_supported_cxx_standard(self):
-        cxx17 = "-std=c++17"
-        cxx14 = "-std=gnu++14"
-        cxx11 = "-std=c++11"
-        if self.test_supports_compile_arg(cxx17):
-            for e in self.extensions:
-                e.extra_compile_args.append(cxx17)
-        elif self.test_supports_compile_arg(cxx14):
-            for e in self.extensions:
-                e.extra_compile_args.append(cxx14)
-        elif self.test_supports_compile_arg(cxx11):
-            for e in self.extensions:
-                e.extra_compile_args.append(cxx11)
-        else:
+        args_cxx_std = ["-std=c++17", "-std=gnu++14", "-std=c++11"]
+        set_standard = False
+        for arg_cxx_std in args_cxx_std:
+            if self.test_supports_compile_arg(arg_cxx_std):
+                for e in self.extensions:
+                    e.extra_compile_args.append(arg_cxx_std)
+                set_standard = True
+                break
+        if not set_standard:
             msg  = "\n\n\nWarning: compiler does not support C++11, compilation/installation of "
             msg += "'isotree' might fail without it.\n\n\n"
             warnings.warn(msg)
@@ -203,13 +246,12 @@ class build_ext_subclass( build_ext ):
                     cmd = self.compiler.compiler_cxx
             except Exception:
                 cmd = self.compiler.compiler_cxx
-            val_good = subprocess.call(cmd + [fname])
             if with_omp:
                 with open(fname, "w") as ftest:
                     ftest.write(u"#include <omp.h>\nint main(int argc, char**argv) {return 0;}\n")
             try:
-                val = subprocess.call(cmd + comm + [fname])
-                is_supported = (val == val_good)
+                val = subprocess.run(cmd + comm + [fname], capture_output=silent_tests).returncode
+                is_supported = (val == EXIT_SUCCESS)
             except Exception:
                 is_supported = False
         except Exception:
@@ -219,16 +261,6 @@ class build_ext_subclass( build_ext ):
         except Exception:
             pass
         return is_supported
-
-    def is_arch_in_cflags(self):
-        arch_flags = '-march -mtune -msse -msse2 -msse3 -mssse3 -msse4 -msse4a -msse4.1 -msse4.2 -mavx -mavx2 -mcpu'.split()
-        for env_var in ("CFLAGS", "CXXFLAGS"):
-            if env_var in os.environ:
-                for flag in arch_flags:
-                    if flag in os.environ[env_var]:
-                        return True
-
-        return False
 
     def add_restrict_qualifier(self):
         supports_restrict = False
@@ -246,12 +278,11 @@ class build_ext_subclass( build_ext ):
                     cmd = self.compiler.compiler_cxx
             except Exception:
                 cmd = self.compiler.compiler_cxx
-            val_good = subprocess.call(cmd + [fname])
             try:
                 with open(fname, "w") as ftest:
                     ftest.write(u"int main(int argc, char**argv) {double *__restrict x = 0; return 0;}\n")
-                val = subprocess.call(cmd + [fname])
-                supports_restrict = (val == val_good)
+                val = subprocess.run(cmd + comm + [fname], capture_output=silent_tests).returncode
+                is_supported = (val == EXIT_SUCCESS)
             except Exception:
                 return None
         except Exception:
@@ -269,10 +300,9 @@ class build_ext_subclass( build_ext ):
 setup(
     name  = "isotree",
     packages = ["isotree"],
-    version = '0.5.19',
+    version = '0.5.19-1',
     description = 'Isolation-Based Outlier Detection, Distance, and NA imputation',
     author = 'David Cortes',
-    author_email = 'david.cortes.rivera@gmail.com',
     url = 'https://github.com/david-cortes/isotree',
     keywords = ['isolation-forest', 'anomaly', 'outlier'],
     cmdclass = {'build_ext': build_ext_subclass},
@@ -286,7 +316,8 @@ setup(
                                 language="c++",
                                 install_requires = ["numpy", "pandas>=0.24.0", "cython", "scipy"],
                                 define_macros = [("_USE_XOSHIRO", None),
-                                                 ("_FOR_PYTHON", None)]
+                                                 ("NDEBUG", None),
+                                                 ("_FOR_PYTHON", None),]
                             )]
     )
 
@@ -298,5 +329,5 @@ if not found_omp:
     else:
         omp_msg += " modules for your compiler. "
     
-    omp_msg += "Then reinstall this package from scratch: 'pip install --force-reinstall isotree'.\n"
+    omp_msg += "Then reinstall this package from scratch: 'pip install --upgrade --no-deps --force-reinstall isotree'.\n"
     warnings.warn(omp_msg)
