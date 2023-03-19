cfa02dfa9589b3905e5599d2e2ee83e3214f1c7d
david-cortes <david.cortes.rivera@gmail.com>
2023-03-19 20:31:03 +0100

    fixes for apple systems

--- setup.py
+++ setup.py
@@ -125,7 +125,7 @@ class build_ext_subclass( build_ext ):
     def add_march_native(self):
         args_march_native = ["-march=native", "-mcpu=native"]
         for arg_march_native in args_march_native:
-            if self.test_supports_compile_arg(arg_march_native):
+            if self.test_supports_compile_arg(arg_march_native, with_c_comp=True):
                 for e in self.extensions:
                     e.extra_compile_args.append(arg_march_native)
                 break
@@ -164,7 +164,7 @@ class build_ext_subclass( build_ext ):
         args_apple_omp2 = ["-Xclang", "-fopenmp", "-L/usr/local/lib", "-lomp", "-I/usr/local/include"]
         has_brew_omp = False
         if is_apple:
-            res_brew_pref = subprocess.run(["brew", "--prefix", "libomp"], capture_output=silent_tests)
+            res_brew_pref = subprocess.run(["brew", "--prefix", "libomp"], capture_output=True)
             if res_brew_pref.returncode == EXIT_SUCCESS:
                 has_brew_omp = True
                 brew_omp_prefix = res_brew_pref.stdout.decode().strip()
@@ -228,7 +228,10 @@ class build_ext_subclass( build_ext ):
             msg += "'isotree' might fail without it.\n\n\n"
             warnings.warn(msg)
 
-    def test_supports_compile_arg(self, comm, with_omp=False):
+    # Note: in apple systems, it somehow might end up triggering the arguments with
+    # the C compiler instead of the CXX compiler. What's worse, sometimes this compiler
+    # thinks it's building for aarch64 even when executed in amd64.
+    def test_supports_compile_arg(self, comm, with_omp=False, with_c_comp=False):
         is_supported = False
         try:
             if not hasattr(self.compiler, "compiler_cxx"):
@@ -246,12 +249,20 @@ class build_ext_subclass( build_ext ):
                     cmd = self.compiler.compiler_cxx
             except Exception:
                 cmd = self.compiler.compiler_cxx
+            if with_c_comp:
+                if not isinstance(self.compiler.compiler, list):
+                    cmd0 = list(self.compiler.compiler)
+                else:
+                    cmd0 = self.compiler.compiler
             if with_omp:
                 with open(fname, "w") as ftest:
                     ftest.write(u"#include <omp.h>\nint main(int argc, char**argv) {return 0;}\n")
             try:
                 val = subprocess.run(cmd + comm + [fname], capture_output=silent_tests).returncode
                 is_supported = (val == EXIT_SUCCESS)
+                if is_supported and with_c_comp:
+                    val = subprocess.run(cmd0 + comm + [fname], capture_output=silent_tests).returncode
+                    is_supported = (val == EXIT_SUCCESS)
             except Exception:
                 is_supported = False
         except Exception:
@@ -300,7 +311,7 @@ class build_ext_subclass( build_ext ):
 setup(
     name  = "isotree",
     packages = ["isotree"],
-    version = '0.5.19-1',
+    version = '0.5.19-2',
     description = 'Isolation-Based Outlier Detection, Distance, and NA imputation',
     author = 'David Cortes',
     url = 'https://github.com/david-cortes/isotree',
