d410e00c868adfcc1119563204fb1384cbd179a8
david-cortes <david.cortes.rivera@gmail.com>
2023-03-25 17:59:40 +0100

    more apple fixes

--- setup.py
+++ setup.py
@@ -165,11 +165,14 @@ class build_ext_subclass( build_ext ):
         args_apple_omp2 = ["-Xclang", "-fopenmp", "-L/usr/local/lib", "-lomp", "-I/usr/local/include"]
         has_brew_omp = False
         if is_apple:
-            res_brew_pref = subprocess.run(["brew", "--prefix", "libomp"], capture_output=True)
-            if res_brew_pref.returncode == EXIT_SUCCESS:
-                has_brew_omp = True
-                brew_omp_prefix = res_brew_pref.stdout.decode().strip()
-                args_apple_omp3 = ["-Xclang", "-fopenmp", f"-L{brew_omp_prefix}/lib", "-lomp", f"-I{brew_omp_prefix}/include"]
+            try:
+                res_brew_pref = subprocess.run(["brew", "--prefix", "libomp"], capture_output=True)
+                if res_brew_pref.returncode == EXIT_SUCCESS:
+                    brew_omp_prefix = res_brew_pref.stdout.decode().strip()
+                    args_apple_omp3 = ["-Xclang", "-fopenmp", f"-L{brew_omp_prefix}/lib", "-lomp", f"-I{brew_omp_prefix}/include"]
+                    has_brew_omp = True
+            except Exception as e:
+                pass
 
 
         if self.test_supports_compile_arg(arg_omp1, with_omp=True):
@@ -312,7 +315,7 @@ class build_ext_subclass( build_ext ):
 setup(
     name  = "isotree",
     packages = ["isotree"],
-    version = '0.5.19-3',
+    version = '0.5.19-4',
     description = 'Isolation-Based Outlier Detection, Distance, and NA imputation',
     author = 'David Cortes',
     url = 'https://github.com/david-cortes/isotree',
